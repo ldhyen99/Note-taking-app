53b7c8f2d2cded2b74edc62331adfd4e11701977
unknown <ldhyen99@gmail.com>
2021-02-18 15:18:43 +0700

    update vercel file

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !==
     'production'
 
-export const server = dev ? 'http://localhost:3000' : 'note-taking-app.now.sh'
+export const server = dev ? 'http://localhost:3000' : 'https://note-taking-app.now.sh'
 // null is your website domain
