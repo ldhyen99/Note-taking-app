c84768d0cd0a0bcfea04621beff743b698b901fd
unknown <ldhyen99@gmail.com>
2021-02-18 15:47:46 +0700

    update

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !==
     'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://note-taking-app.now.sh'
+export const server = dev ? 'http://localhost:3000' : 'https://note-taking-app-5udjynrjd.vercel.app'
 // null is your website domain
