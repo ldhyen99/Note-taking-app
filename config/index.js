6d484a95602d430c0ce4aadb48b7452625bfb92c
unknown <ldhyen99@gmail.com>
2021-02-18 14:55:49 +0700

    update config

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !==
     'production'
 
-export const server = dev ? 'http://localhost:3000' : null
+export const server = dev ? 'http://localhost:3000' : 'note-taking-app.now.sh'
 // null is your website domain
