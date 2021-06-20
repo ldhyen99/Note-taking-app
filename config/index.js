b8b4eb3cdefed664f4094e5b4e749532b1a690e2
@yenldh <yenldh>
2021-06-20 16:42:08 +0700

    update server

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !==
     'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://myweb.com'
+export const server = dev ? 'http://localhost:3000' : 'https://note-taking-app.vercel.app'
 // null is your website domain
